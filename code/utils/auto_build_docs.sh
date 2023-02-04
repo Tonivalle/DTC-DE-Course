de7ccc0629adcc244d512a186b647e4222360594
Antonio <Tonivalle15@gmail.com>
2023-02-04 20:46:54 +0100

    Added auto deploy hook

---
+++ code/utils/auto_build_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Checking for docuntantation changes..."
+
+DOC_CHANGES=$(git diff --cached --name-only --diff-filter=ACM | grep docs/ | wc -l | sed 's/^ *//g')
+
+if [ $DOC_CHANGES -gt 0 ]
+then
+    echo "$DOC_CHANGES doc files changed. Deploying to github pages..."
+    # mkdocs gh-deploy
+else
+    echo "No new changes to deploy!"
+fi
